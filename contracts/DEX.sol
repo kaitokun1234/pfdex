20099f9831784407e2ade5d63f9a985d3b668e75
kaikun <kiruazr@gmail.com>
2022-04-10 05:16:14 +0900

    first commit

---
+++ contracts/DEX.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./JPYCoin.sol";
+
+contract Dex {
+    event buy(address acount, address _tokenAddr, uint256 _cost, uint256 _amount);
+    event sell(address acount, address _tokenAddr, uint256 _cost, uint256 _amount);
+
+    mapping(address => bool) public supportedTokenAddr;
+
+    modifier supportsToken(address _tokenAddr){
+        require(supportedTokenAddr[_tokenAddr] == true, "This token is not supported");
+        _;
+    }
+
+    constructor(address[] memory _tokenAddr){
+        for(uint i=0; i< _tokenAddr.length; i++){
+            supportedTokenAddr[_tokenAddr[i]] = true;
+        }
+    }
+    function buyToken(address _tokenAddr, uint256 _cost, uint256 _amount) external payable supportsToken(_tokenAddr) {
+        ERC20 token = ERC20(_tokenAddr);
+        require(msg.value == _cost, "Insufficient fund");
+        require(token.balanceOf(address(this)) >= _amount, "Token sold out");
+        token.transfer(msg.sender, _amount);
+        emit buy(msg.sender, _tokenAddr, _cost, _amount);
+    }
+
+    function sellToken(address _tokenAddr, uint256 _cost, uint256 _amount) external supportsToken(_tokenAddr){
+        ERC20 token = ERC20(_tokenAddr);
+        require(token.balanceOf(msg.sender) >= _cost, "Insufficient token balance");
+        require(address(this).balance >= _amount, "Dex dows not have enough funds");
+        token.transferFrom(msg.sender, address(this), _cost);
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "ETH transfer failed");
+        emit sell(msg.sender, _tokenAddr, _cost, _amount);
+    }
+}
